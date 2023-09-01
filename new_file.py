739dd2224e316c8efcf2a400cb33eb137ea3b28f
Sara Meriem Abdelhafid <sara23.abdelhafid@gmail.com>
2023-09-01 16:52:49 +0100

    change1 project new file

--- new_file.py
+++ new_file.py
@@ -1,6 +1,6 @@
 print('hello word')
 
-print('hello word')
-print('hello word')
-print('hello word')
-print('hello word')
+print('hello word 1000')
+print('hello word 2000')
+print('hello word 3000')
+print('hello word 4000')
